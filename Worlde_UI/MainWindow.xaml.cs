fc27388ac4b52df669f891e489fa15eb091e6b22
david <davidws13@outlook.com>
2023-10-24 11:26:35 +0200

    Worlde_UI push

---
+++ Worlde_UI/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Worlde_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> words = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Vælg_fil_knap_Click(object sender, RoutedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+                if(filePath != string.Empty)
+                {
+                    Path_label.Text = System.IO.Path.GetFileName(filePath);
+                }
+
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        words.Add(reader.ReadLine());
+                    }
+                }
+            }
+        }
+    }
+}
